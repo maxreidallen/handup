391fc00f6f0d310e2024b67353d9669584529b44
Max R. Allen <maxreidallen@gmail.com>
2013-03-16 13:32:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handup::Application.config.secret_token = '987c37e326cc6d1106cb76c9d09afdc69feb98ceca434d4de66c2dc691b76b9ff2f4b712aef335b105a1277d80a5e03ff13ff095fd5152bf394dbae8afc7f08b'
